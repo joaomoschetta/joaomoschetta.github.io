d6cef8375302798fb89979cf5afb3e240b6d96dc
joaomoschetta <joaopedromschneider123@gmail.com>
2021-08-07 17:19:30 -0300

    Pages folder structure fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import { About } from './pages/About';
-import { Projects } from './pages/Projects';
-import { Contact } from './pages/Contact';
-import { Admin } from './pages/Admin';
+import { About } from './pages/About/index';
+import { Projects } from './pages/Projects/index';
+import { Contact } from './pages/Contact/index';
+import { Admin } from './pages/Admin/index';
 
 function App() {
   return (
