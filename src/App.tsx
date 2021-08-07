a103753002ae326d431e469dab7dba7a78bd9144
joaomoschetta <joaopedromschneider123@gmail.com>
2021-08-07 17:05:35 -0300

    Routing + pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import { About } from './pages/About';
+import { Projects } from './pages/Projects';
+import { Contact } from './pages/Contact';
+import { Admin } from './pages/Admin';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Joao Moschetta</h1>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route
+          path='/projetos'
+          exact
+          component={Projects}
+        />
+        <Route
+          path='/contato'
+          exact
+          component={Contact}
+        />
+        <Route
+          path='/admin'
+          exact
+          component={Admin}
+        />
+        <Route
+          path='/'
+          component={About}
+        />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Contact.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function Contact() {
+  return (
+    <div>CONTACT</div>
+  );
+}
