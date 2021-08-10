cb3cccab74a9945fd29873c63be540ec0985c286
joaomoschetta <joaopedromschneider123@gmail.com>
2021-08-09 21:53:37 -0300

    Added NavBar component at App + Route fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,66 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  RouteProps,
+  Redirect
+} from 'react-router-dom';
 
 import { About } from './pages/About/index';
 import { Projects } from './pages/Projects/index';
 import { Contact } from './pages/Contact/index';
 import { Admin } from './pages/Admin/index';
+import { NavBar } from './components/NavBar';
 
 function App() {
+  const routes : RouteProps[] = [
+    {
+      path: '/sobre',
+      exact: true,
+      component: About,
+    },
+    {
+      path: '/projetos',
+      exact: true,
+      component: Projects,
+    },
+    {
+      path: '/contato',
+      exact: true,
+      component: Contact,
+    },
+    {
+      path: '/admin',
+      exact: true,
+      component: Admin,
+    },
+    {
+      path: '/',
+      component: redirectToAboutPage,
+    },
+  ]
+
+  function redirectToAboutPage() {
+    return (
+      <Redirect to="/sobre" />
+    );
+  }
+
   return (
     <BrowserRouter>
+      <NavBar routes={routes} />
+
       <Switch>
-        <Route
-          path='/projetos'
-          exact
-          component={Projects}
-        />
-        <Route
-          path='/contato'
-          exact
-          component={Contact}
-        />
-        <Route
-          path='/admin'
-          exact
-          component={Admin}
-        />
-        <Route
-          path='/'
-          component={About}
-        />
+        {routes.map((route, index) => {
+          return (
+            <Route
+              path={route.path}
+              exact={route.exact}
+              component={route.component}
+            />
+          );
+        })}
       </Switch>
     </BrowserRouter>
   );
