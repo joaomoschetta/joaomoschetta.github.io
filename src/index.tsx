212dcb54ae933c47e2f0028114b3373e3d4def83
joaomoschetta <joaopedromschneider123@gmail.com>
2021-08-11 23:21:55 -0300

    Added scss general styles (reset + fonts + colors)

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import './styles/reset.scss';
+import './styles/fonts.scss';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
