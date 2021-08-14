89c9ee9628e53dfdcd2f4caf1204ab4591935385
joaomoschetta <joaopedromschneider123@gmail.com>
2021-08-14 16:30:01 -0300

    Style fix: About page

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -36,37 +36,35 @@ export function About() {
   ];
 
   return (
-    <div className="about-page">
-      <main>
-        <section className="titles-and-links">
-          <h1>João Moschetta</h1>
-          <h2>Desenvolvedor Front-end</h2>
-          <ul>
-            {contactLinks.map((contactLink, index) => {
-              return (
-                <li key={index} className={contactLink.name}>
-                  <a
-                    href={contactLink.link}
-                    target="_blank"
-                    rel="noreferrer nofollow noopener external"
-                  >
-                    <img
-                      src={contactLink.image}
-                      alt={contactLink.titleAndAltText}
-                      title={contactLink.titleAndAltText}
-                    />
-                  </a>
-                </li>
-              );
-            })}
-          </ul>
-        </section>
+    <main className="about-page">
+      <section className="titles-and-links">
+        <h1>João Moschetta</h1>
+        <h2>Desenvolvedor Front-end</h2>
+        <ul>
+          {contactLinks.map((contactLink, index) => {
+            return (
+              <li key={index} className={contactLink.name}>
+                <a
+                  href={contactLink.link}
+                  target="_blank"
+                  rel="noreferrer nofollow noopener external"
+                >
+                  <img
+                    src={contactLink.image}
+                    alt={contactLink.titleAndAltText}
+                    title={contactLink.titleAndAltText}
+                  />
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
 
-        <section className="about">
-          <h3>SOBRE</h3>
-          <p>Sempre fui apaixonado por tecnologia, tive meu primeiro contato com programação em 2018 e desde então nao parei de estudar. Sou auto-didata e atualmente me aventuro pelo desenvolvimento Web, buscando sempre mais experiencia para evoluir a cada dia.</p>
-        </section>
-      </main>
-    </div>
+      <section className="about">
+        <h3>SOBRE</h3>
+        <p>Sempre fui apaixonado por tecnologia, tive meu primeiro contato com programação em 2018 e desde então nao parei de estudar. Sou auto-didata e atualmente me aventuro pelo desenvolvimento Web, buscando sempre mais experiencia para evoluir a cada dia.</p>
+      </section>
+    </main>
   );
 }
