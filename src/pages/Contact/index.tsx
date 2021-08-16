6cfe554cae59492a64e5deb3b719551cd523a1fb
joaomoschetta <joaopedromschneider123@gmail.com>
2021-08-15 22:11:50 -0300

    Added Contact page + style

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,7 +1,50 @@
 import React from "react";
 
+import { contacts } from '../../data/contacts';
+
+import './style.scss';
+
 export function Contact() {
+  
   return (
-    <div>CONTACT</div>
+    <main className="contact-page">
+      <h3>CONTATO</h3>
+
+      <ul>
+        {contacts.map((contact) => {
+          return (
+            <li key={contact.id}>
+              <a
+                href={contact.link}
+                target="_blank"
+                rel="noreferrer nofollow noopener external"
+              >
+                <section className="top-row">
+                  {contact.icon && (
+                    <div className="img-container">
+                      <img src={contact.icon} alt="" />
+                    </div>
+                  )}
+
+                  <strong
+                    className={contact.icon ? 'hasIcon' : ''}
+                  >{contact.title}</strong>
+                </section>
+
+                {(contact.text || contact.adicionalTitle) && (<section className="down-row">
+                  {contact.text && (
+                    <p>{contact.text}</p>
+                  )}
+                  
+                  {contact.adicionalTitle && (
+                    <strong>{contact.adicionalTitle}</strong>
+                  )}
+                </section>)}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </main>
   );
 }
