35e21073c079b06e18f275ba5f03743d12b657ef
joaomoschetta <joaopedromschneider123@gmail.com>
2021-08-16 23:53:07 -0300

    Added Projects page + style

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -1,7 +1,41 @@
 import React from "react";
+import { useState } from "react";
+
+import { projects, IProjects } from '../../data/projects';
+
+import './style.scss';
 
 export function Projects() {
+  const [isModalActive, setIsModalActive] = useState<boolean>(false);
+  const [modalProject, setModalProject] = useState<IProjects | object>(projects[0] || {});
+
+  function handleOpenModal(project : IProjects) {
+    setModalProject(project);
+    setIsModalActive(true);
+  }
+
   return (
-    <div>PROJECTS</div>
+    <main className="projects-page">
+      <h3>PROJETOS</h3>
+
+      <ul>
+        {projects.map((project, index) => (
+          <li key={index}>
+            <img src={project.image} alt={`${project.name} showcase`} />
+            <div className="about">
+              <strong>{project.name}</strong>
+              <p>{project.summary}</p>
+              <button onClick={() => handleOpenModal(project)}>
+                Mais detalhes
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+
+      {/* {isModalActive && (
+        <Modal />
+      )} */}
+    </main>
   );
 }
